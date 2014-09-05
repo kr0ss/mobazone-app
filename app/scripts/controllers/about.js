e73be58ed927be250505190fd1234b1f526f2ce5
Cristian Quiroz <cris.quiroz89@gmail.com>
2014-09-05 11:54:23 -0400

    Implementa Estructura Base

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mobazoneAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mobazoneAppApp
+ */
+angular.module('mobazoneAppApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mobazoneAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mobazoneAppApp
+ */
+angular.module('mobazoneAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
